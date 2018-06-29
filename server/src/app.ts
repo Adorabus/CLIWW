933702a2c8042a13d335ed639c0c9edbcecfa44e
Adorabus <adorabusfox@gmail.com>
2018-06-29 01:06:35 -0700

    Remove unused dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,27 +1,29 @@
 import * as express from 'express'
 import * as http from 'http'
 import * as socketIO from 'socket.io'
-import * as bodyParser from 'body-parser'
 import Wrapper from './wrapper'
 import Messenger from './messenger'
 import {AddressInfo} from 'net'
+import * as yargs from 'yargs'
+
+const argv = yargs
+  .usage('Usage: $0 [options] <command>')
+  .option('password', {
+    alias: 'p',
+    describe: 'Console access password.'
+  })
+  .demandCommand(1, 'No command specified.')
+  .argv
 
 const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 
-app.use(bodyParser.json())
-
-const idx = process.argv.indexOf('--cmd')
-if (idx === -1) {
-  console.error('Missing launch command! (--cmd command [arg1, ...])')
-  process.exit(1)
-}
-
-const args = process.argv.splice(idx + 1, process.argv.length) as string[]
-const command = args.shift() as string
-
-const wrapper = new Wrapper({command, args})
+const command = argv._.shift() as string
+const wrapper = new Wrapper({
+  command,
+  args: argv.__
+})
 const messenger = new Messenger(io, wrapper)
 
 app.get('/console', (req, res) => {
